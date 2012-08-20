c836f9f5972f2e8b5f4b49cef3fd19eead9492c0
Frode Danielsen <frode@e5r.no>
2012-08-20 15:48:13 +0200

    Minor version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-taggit-autocomplete-jqueryui',
-    version='0.1dev',
+    version='0.2dev',
     packages=['taggit_autocomplete_jqueryui', ],
     license='BSD License',
     long_description=open('README.txt').read()
