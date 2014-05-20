33d98dd57f61f3495aaaebc8dbdf60a7a4465653
Frode Danielsen <frode@e5r.no>
2014-05-20 16:52:54 +0200

    Make sure static files gets installed

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='django-taggit-autocomplete-jqueryui',
-    version='0.3dev',
-    packages=['taggit_autocomplete_jqueryui', ],
+    version='0.3.1dev',
+    packages=find_packages(),
+    include_package_data=True,
     license='BSD License',
     long_description=open('README.txt').read()
 )
