1f8178fb185ac1e56de42f2fb4d27334996ddd9d
Simen Heggestøyl <simen@e5r.no>
2017-05-02 11:51:07 +0200

    Release version 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='django-taggit-autocomplete-jqueryui',
-    version='0.3.1dev',
+    version='0.4.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
